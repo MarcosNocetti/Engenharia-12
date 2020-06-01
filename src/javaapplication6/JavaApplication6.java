a2616d50b6d21877020b02156d50f60c09b7befd
eric satoshi <satoshi.eric@gmail.com>
2020-06-01 20:19:58 -0300

    novas funcionalidades adicionadas

--- src/javaapplication6/JavaApplication6.java
+++ src/javaapplication6/JavaApplication6.java
@@ -11,11 +11,15 @@ package javaapplication6;
  */
 public class JavaApplication6 {
 
+    public static int somar(int x, int y){
+        return x + y;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
